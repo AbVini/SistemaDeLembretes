8b379ea1b9673c143f94ff6b877dc74f5ca4cb61
AbVini <abraao.vini16@gmail.com>
2024-07-09 19:04:21 -0300

    fix

--- Backend/LembretesAPI/Entities/Lembretes.cs
+++ Backend/LembretesAPI/Entities/Lembretes.cs
@@ -1,9 +1,18 @@
-﻿namespace LembretesAPI.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using LembretesAPI.Validacoes;
+
+namespace LembretesAPI.Entities
 {
     public class Lembrete
     {
+        [Key]
         public int Id { get; set; }
-        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Este Campo e obrigario")]
+        public required string Titulo { get; set; }
+
+        [Required(ErrorMessage ="A data deve ser informada")]
+        [ValidacaoDataAtual(ErrorMessage = "A data deve ser maior ou igual a data atual")]
         public DateTime Data { get; set; }
 
     }
---
+++ Backend/LembretesAPI/Validacoes/ValidarDataAtualAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LembretesAPI.Validacoes;
+
+public class ValidacaoDataAtualAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        DateTime data = (DateTime)value;
+
+
+        if (data < DateTime.Today)
+        {
+            return new ValidationResult(ErrorMessage);
+        }
+
+        return ValidationResult.Success;
+    }
+}
